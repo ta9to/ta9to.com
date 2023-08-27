48ec56f9cb39c2fc9a31f6f56ef1612854fb9854
ta9to <ta9to.kd@gmail.com>
2023-08-27 16:17:26 +0900

    add article

--- typography.js
+++ typography.js
@@ -1,221 +1,227 @@
 module.exports = ({ theme }) => ({
+  invert: {
+    css: {
+      '--tw-prose-body': 'var(--tw-prose-invert-body)',
+      '--tw-prose-headings': 'var(--tw-prose-invert-headings)',
+      '--tw-prose-links': 'var(--tw-prose-invert-links)',
+      '--tw-prose-links-hover': 'var(--tw-prose-invert-links-hover)',
+      '--tw-prose-underline': 'var(--tw-prose-invert-underline)',
+      '--tw-prose-underline-hover': 'var(--tw-prose-invert-underline-hover)',
+      '--tw-prose-bold': 'var(--tw-prose-invert-bold)',
+      '--tw-prose-counters': 'var(--tw-prose-invert-counters)',
+      '--tw-prose-bullets': 'var(--tw-prose-invert-bullets)',
+      '--tw-prose-hr': 'var(--tw-prose-invert-hr)',
+      '--tw-prose-quote-borders': 'var(--tw-prose-invert-quote-borders)',
+      '--tw-prose-captions': 'var(--tw-prose-invert-captions)',
+      '--tw-prose-code': 'var(--tw-prose-invert-code)',
+      '--tw-prose-code-bg': 'var(--tw-prose-invert-code-bg)',
+      '--tw-prose-pre-code': 'var(--tw-prose-invert-pre-code)',
+      '--tw-prose-pre-bg': 'var(--tw-prose-invert-pre-bg)',
+      '--tw-prose-pre-border': 'var(--tw-prose-invert-pre-border)',
+      '--tw-prose-th-borders': 'var(--tw-prose-invert-th-borders)',
+      '--tw-prose-td-borders': 'var(--tw-prose-invert-td-borders)',
+    },
+  },
   DEFAULT: {
     css: {
-      '--tw-prose-body': theme('colors.zinc.700'),
+      '--tw-prose-body': theme('colors.zinc.600'),
       '--tw-prose-headings': theme('colors.zinc.900'),
-      '--tw-prose-links': theme('colors.emerald.500'),
-      '--tw-prose-links-hover': theme('colors.emerald.600'),
-      '--tw-prose-links-underline': theme('colors.emerald.500 / 0.3'),
+      '--tw-prose-links': theme('colors.teal.500'),
+      '--tw-prose-links-hover': theme('colors.teal.600'),
+      '--tw-prose-underline': theme('colors.teal.500 / 0.2'),
+      '--tw-prose-underline-hover': theme('colors.teal.500'),
       '--tw-prose-bold': theme('colors.zinc.900'),
-      '--tw-prose-counters': theme('colors.zinc.500'),
-      '--tw-prose-bullets': theme('colors.zinc.300'),
-      '--tw-prose-hr': theme('colors.zinc.900 / 0.05'),
-      '--tw-prose-quotes': theme('colors.zinc.900'),
+      '--tw-prose-counters': theme('colors.zinc.900'),
+      '--tw-prose-bullets': theme('colors.zinc.900'),
+      '--tw-prose-hr': theme('colors.zinc.100'),
       '--tw-prose-quote-borders': theme('colors.zinc.200'),
-      '--tw-prose-captions': theme('colors.zinc.500'),
-      '--tw-prose-code': theme('colors.zinc.900'),
-      '--tw-prose-code-bg': theme('colors.zinc.100'),
-      '--tw-prose-code-ring': theme('colors.zinc.300'),
-      '--tw-prose-th-borders': theme('colors.zinc.300'),
-      '--tw-prose-td-borders': theme('colors.zinc.200'),
+      '--tw-prose-captions': theme('colors.zinc.400'),
+      '--tw-prose-code': theme('colors.zinc.700'),
+      '--tw-prose-code-bg': theme('colors.zinc.300 / 0.2'),
+      '--tw-prose-pre-code': theme('colors.zinc.100'),
+      '--tw-prose-pre-bg': theme('colors.zinc.900'),
+      '--tw-prose-pre-border': 'transparent',
+      '--tw-prose-th-borders': theme('colors.zinc.200'),
+      '--tw-prose-td-borders': theme('colors.zinc.100'),
 
       '--tw-prose-invert-body': theme('colors.zinc.400'),
-      '--tw-prose-invert-headings': theme('colors.white'),
-      '--tw-prose-invert-links': theme('colors.emerald.400'),
-      '--tw-prose-invert-links-hover': theme('colors.emerald.500'),
-      '--tw-prose-invert-links-underline': theme('colors.emerald.500 / 0.3'),
-      '--tw-prose-invert-bold': theme('colors.white'),
-      '--tw-prose-invert-counters': theme('colors.zinc.400'),
-      '--tw-prose-invert-bullets': theme('colors.zinc.600'),
-      '--tw-prose-invert-hr': theme('colors.white / 0.05'),
-      '--tw-prose-invert-quotes': theme('colors.zinc.100'),
-      '--tw-prose-invert-quote-borders': theme('colors.zinc.700'),
-      '--tw-prose-invert-captions': theme('colors.zinc.400'),
-      '--tw-prose-invert-code': theme('colors.white'),
-      '--tw-prose-invert-code-bg': theme('colors.zinc.700 / 0.15'),
-      '--tw-prose-invert-code-ring': theme('colors.white / 0.1'),
-      '--tw-prose-invert-th-borders': theme('colors.zinc.600'),
-      '--tw-prose-invert-td-borders': theme('colors.zinc.700'),
+      '--tw-prose-invert-headings': theme('colors.zinc.200'),
+      '--tw-prose-invert-links': theme('colors.teal.400'),
+      '--tw-prose-invert-links-hover': theme('colors.teal.400'),
+      '--tw-prose-invert-underline': theme('colors.teal.400 / 0.3'),
+      '--tw-prose-invert-underline-hover': theme('colors.teal.400'),
+      '--tw-prose-invert-bold': theme('colors.zinc.200'),
+      '--tw-prose-invert-counters': theme('colors.zinc.200'),
+      '--tw-prose-invert-bullets': theme('colors.zinc.200'),
+      '--tw-prose-invert-hr': theme('colors.zinc.700 / 0.4'),
+      '--tw-prose-invert-quote-borders': theme('colors.zinc.500'),
+      '--tw-prose-invert-captions': theme('colors.zinc.500'),
+      '--tw-prose-invert-code': theme('colors.zinc.300'),
+      '--tw-prose-invert-code-bg': theme('colors.zinc.200 / 0.05'),
+      '--tw-prose-invert-pre-code': theme('colors.zinc.100'),
+      '--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 0.4)',
+      '--tw-prose-invert-pre-border': theme('colors.zinc.200 / 0.1'),
+      '--tw-prose-invert-th-borders': theme('colors.zinc.700'),
+      '--tw-prose-invert-td-borders': theme('colors.zinc.800'),
 
       // Base
       color: 'var(--tw-prose-body)',
-      fontSize: theme('fontSize.sm')[0],
       lineHeight: theme('lineHeight.7'),
-
-      // Layout
       '> *': {
-        maxWidth: theme('maxWidth.2xl'),
-        marginLeft: 'auto',
-        marginRight: 'auto',
-        '@screen lg': {
-          maxWidth: theme('maxWidth.3xl'),
-          marginLeft: `calc(50% - min(50%, ${theme('maxWidth.lg')}))`,
-          marginRight: `calc(50% - min(50%, ${theme('maxWidth.lg')}))`,
-        },
+        marginTop: theme('spacing.10'),
+        marginBottom: theme('spacing.10'),
+      },
+      p: {
+        marginTop: theme('spacing.7'),
+        marginBottom: theme('spacing.7'),
       },
 
-      // Text
-      p: {
-        marginTop: theme('spacing.6'),
-        marginBottom: theme('spacing.6'),
+      // Headings
+      'h2, h3': {
+        color: 'var(--tw-prose-headings)',
+        fontWeight: theme('fontWeight.semibold'),
       },
-      '[class~="lead"]': {
+      h2: {
+        fontSize: theme('fontSize.xl')[0],
+        lineHeight: theme('lineHeight.7'),
+        marginTop: theme('spacing.20'),
+        marginBottom: theme('spacing.4'),
+      },
+      h3: {
         fontSize: theme('fontSize.base')[0],
-        ...theme('fontSize.base')[1],
+        lineHeight: theme('lineHeight.7'),
+        marginTop: theme('spacing.16'),
+        marginBottom: theme('spacing.4'),
+      },
+      ':is(h2, h3) + *': {
+        marginTop: 0,
+      },
+
+      // Images
+      img: {
+        borderRadius: theme('borderRadius.3xl'),
+      },
+
+      // Inline elements
+      a: {
+        color: 'var(--tw-prose-links)',
+        fontWeight: theme('fontWeight.semibold'),
+        textDecoration: 'underline',
+        textDecorationColor: 'var(--tw-prose-underline)',
+        transitionProperty: 'color, text-decoration-color',
+        transitionDuration: theme('transitionDuration.150'),
+        transitionTimingFunction: theme('transitionTimingFunction.in-out'),
+      },
+      'a:hover': {
+        color: 'var(--tw-prose-links-hover)',
+        textDecorationColor: 'var(--tw-prose-underline-hover)',
+      },
+      strong: {
+        color: 'var(--tw-prose-bold)',
+        fontWeight: theme('fontWeight.semibold'),
+      },
+      code: {
+        display: 'inline-block',
+        color: 'var(--tw-prose-code)',
+        fontSize: theme('fontSize.sm')[0],
+        fontWeight: theme('fontWeight.semibold'),
+        backgroundColor: 'var(--tw-prose-code-bg)',
+        borderRadius: theme('borderRadius.lg'),
+        paddingLeft: theme('spacing.1'),
+        paddingRight: theme('spacing.1'),
+      },
+      'a code': {
+        color: 'inherit',
+      },
+      ':is(h2, h3) code': {
+        fontWeight: theme('fontWeight.bold'),
+      },
+
+      // Quotes
+      blockquote: {
+        paddingLeft: theme('spacing.6'),
+        borderLeftWidth: theme('borderWidth.2'),
+        borderLeftColor: 'var(--tw-prose-quote-borders)',
+        fontStyle: 'italic',
+      },
+
+      // Figures
+      figcaption: {
+        color: 'var(--tw-prose-captions)',
+        fontSize: theme('fontSize.sm')[0],
+        lineHeight: theme('lineHeight.6'),
+        marginTop: theme('spacing.3'),
+      },
+      'figcaption > p': {
+        margin: 0,
       },
 
       // Lists
-      ol: {
-        listStyleType: 'decimal',
-        marginTop: theme('spacing.5'),
-        marginBottom: theme('spacing.5'),
-        paddingLeft: '1.625rem',
-      },
-      'ol[type="A"]': {
-        listStyleType: 'upper-alpha',
-      },
-      'ol[type="a"]': {
-        listStyleType: 'lower-alpha',
-      },
-      'ol[type="A" s]': {
-        listStyleType: 'upper-alpha',
-      },
-      'ol[type="a" s]': {
-        listStyleType: 'lower-alpha',
-      },
-      'ol[type="I"]': {
-        listStyleType: 'upper-roman',
-      },
-      'ol[type="i"]': {
-        listStyleType: 'lower-roman',
-      },
-      'ol[type="I" s]': {
-        listStyleType: 'upper-roman',
-      },
-      'ol[type="i" s]': {
-        listStyleType: 'lower-roman',
-      },
-      'ol[type="1"]': {
-        listStyleType: 'decimal',
-      },
       ul: {
         listStyleType: 'disc',
-        marginTop: theme('spacing.5'),
-        marginBottom: theme('spacing.5'),
-        paddingLeft: '1.625rem',
+      },
+      ol: {
+        listStyleType: 'decimal',
+      },
+      'ul, ol': {
+        paddingLeft: theme('spacing.6'),
       },
       li: {
-        marginTop: theme('spacing.2'),
-        marginBottom: theme('spacing.2'),
+        marginTop: theme('spacing.6'),
+        marginBottom: theme('spacing.6'),
+        paddingLeft: theme('spacing[3.5]'),
       },
-      ':is(ol, ul) > li': {
-        paddingLeft: theme('spacing[1.5]'),
+      'li::marker': {
+        fontSize: theme('fontSize.sm')[0],
+        fontWeight: theme('fontWeight.semibold'),
       },
       'ol > li::marker': {
-        fontWeight: '400',
         color: 'var(--tw-prose-counters)',
       },
       'ul > li::marker': {
         color: 'var(--tw-prose-bullets)',
       },
-      '> ul > li p': {
+      'li :is(ol, ul)': {
+        marginTop: theme('spacing.4'),
+        marginBottom: theme('spacing.4'),
+      },
+      'li :is(li, p)': {
         marginTop: theme('spacing.3'),
         marginBottom: theme('spacing.3'),
       },
-      '> ul > li > *:first-child': {
-        marginTop: theme('spacing.5'),
+
+      // Code blocks
+      pre: {
+        color: 'var(--tw-prose-pre-code)',
+        fontSize: theme('fontSize.sm')[0],
+        fontWeight: theme('fontWeight.medium'),
+        backgroundColor: 'var(--tw-prose-pre-bg)',
+        borderRadius: theme('borderRadius.3xl'),
+        padding: theme('spacing.8'),
+        overflowX: 'auto',
+        border: '1px solid',
+        borderColor: 'var(--tw-prose-pre-border)',
       },
-      '> ul > li > *:last-child': {
-        marginBottom: theme('spacing.5'),
-      },
-      '> ol > li > *:first-child': {
-        marginTop: theme('spacing.5'),
-      },
-      '> ol > li > *:last-child': {
-        marginBottom: theme('spacing.5'),
-      },
-      'ul ul, ul ol, ol ul, ol ol': {
-        marginTop: theme('spacing.3'),
-        marginBottom: theme('spacing.3'),
+      'pre code': {
+        display: 'inline',
+        color: 'inherit',
+        fontSize: 'inherit',
+        fontWeight: 'inherit',
+        backgroundColor: 'transparent',
+        borderRadius: 0,
+        padding: 0,
       },
 
       // Horizontal rules
       hr: {
+        marginTop: theme('spacing.20'),
+        marginBottom: theme('spacing.20'),
+        borderTopWidth: '1px',
         borderColor: 'var(--tw-prose-hr)',
-        borderTopWidth: 1,
-        marginTop: theme('spacing.16'),
-        marginBottom: theme('spacing.16'),
-        maxWidth: 'none',
-        marginLeft: `calc(-1 * ${theme('spacing.4')})`,
-        marginRight: `calc(-1 * ${theme('spacing.4')})`,
-        '@screen sm': {
-          marginLeft: `calc(-1 * ${theme('spacing.6')})`,
-          marginRight: `calc(-1 * ${theme('spacing.6')})`,
-        },
         '@screen lg': {
-          marginLeft: `calc(-1 * ${theme('spacing.8')})`,
-          marginRight: `calc(-1 * ${theme('spacing.8')})`,
+          marginLeft: `calc(${theme('spacing.12')} * -1)`,
+          marginRight: `calc(${theme('spacing.12')} * -1)`,
         },
-      },
-
-      // Quotes
-      blockquote: {
-        fontWeight: '500',
-        fontStyle: 'italic',
-        color: 'var(--tw-prose-quotes)',
-        borderLeftWidth: '0.25rem',
-        borderLeftColor: 'var(--tw-prose-quote-borders)',
-        quotes: '"\\201C""\\201D""\\2018""\\2019"',
-        marginTop: theme('spacing.8'),
-        marginBottom: theme('spacing.8'),
-        paddingLeft: theme('spacing.5'),
-      },
-      'blockquote p:first-of-type::before': {
-        content: 'open-quote',
-      },
-      'blockquote p:last-of-type::after': {
-        content: 'close-quote',
-      },
-
-      // Headings
-      h1: {
-        color: 'var(--tw-prose-headings)',
-        fontWeight: '700',
-        fontSize: theme('fontSize.2xl')[0],
-        ...theme('fontSize.2xl')[1],
-        marginBottom: theme('spacing.2'),
-      },
-      h2: {
-        color: 'var(--tw-prose-headings)',
-        fontWeight: '600',
-        fontSize: theme('fontSize.lg')[0],
-        ...theme('fontSize.lg')[1],
-        marginTop: theme('spacing.16'),
-        marginBottom: theme('spacing.2'),
-      },
-      h3: {
-        color: 'var(--tw-prose-headings)',
-        fontSize: theme('fontSize.base')[0],
-        ...theme('fontSize.base')[1],
-        fontWeight: '600',
-        marginTop: theme('spacing.10'),
-        marginBottom: theme('spacing.2'),
-      },
-
-      // Media
-      'img, video, figure': {
-        marginTop: theme('spacing.8'),
-        marginBottom: theme('spacing.8'),
-      },
-      'figure > *': {
-        marginTop: '0',
-        marginBottom: '0',
-      },
-      figcaption: {
-        color: 'var(--tw-prose-captions)',
-        fontSize: theme('fontSize.xs')[0],
-        ...theme('fontSize.xs')[1],
-        marginTop: theme('spacing.2'),
       },
 
       // Tables
@@ -223,9 +229,7 @@ module.exports = ({ theme }) => ({
         width: '100%',
         tableLayout: 'auto',
         textAlign: 'left',
-        marginTop: theme('spacing.8'),
-        marginBottom: theme('spacing.8'),
-        lineHeight: theme('lineHeight.6'),
+        fontSize: theme('fontSize.sm')[0],
       },
       thead: {
         borderBottomWidth: '1px',
@@ -233,24 +237,22 @@ module.exports = ({ theme }) => ({
       },
       'thead th': {
         color: 'var(--tw-prose-headings)',
-        fontWeight: '600',
+        fontWeight: theme('fontWeight.semibold'),
         verticalAlign: 'bottom',
-        paddingRight: theme('spacing.2'),
         paddingBottom: theme('spacing.2'),
+      },
+      'thead th:not(:first-child)': {
         paddingLeft: theme('spacing.2'),
       },
-      'thead th:first-child': {
-        paddingLeft: '0',
-      },
-      'thead th:last-child': {
-        paddingRight: '0',
+      'thead th:not(:last-child)': {
+        paddingRight: theme('spacing.2'),
       },
       'tbody tr': {
         borderBottomWidth: '1px',
         borderBottomColor: 'var(--tw-prose-td-borders)',
       },
       'tbody tr:last-child': {
-        borderBottomWidth: '0',
+        borderBottomWidth: 0,
       },
       'tbody td': {
         verticalAlign: 'baseline',
@@ -264,94 +266,14 @@ module.exports = ({ theme }) => ({
       },
       ':is(tbody, tfoot) td': {
         paddingTop: theme('spacing.2'),
-        paddingRight: theme('spacing.2'),
         paddingBottom: theme('spacing.2'),
+      },
+      ':is(tbody, tfoot) td:not(:first-child)': {
         paddingLeft: theme('spacing.2'),
       },
-      ':is(tbody, tfoot) td:first-child': {
-        paddingLeft: '0',
+      ':is(tbody, tfoot) td:not(:last-child)': {
+        paddingRight: theme('spacing.2'),
       },
-      ':is(tbody, tfoot) td:last-child': {
-        paddingRight: '0',
-      },
-
-      // Inline elements
-      a: {
-        color: 'var(--tw-prose-links)',
-        textDecoration: 'underline transparent',
-        fontWeight: '500',
-        transitionProperty: 'color, text-decoration-color',
-        transitionDuration: theme('transitionDuration.DEFAULT'),
-        transitionTimingFunction: theme('transitionTimingFunction.DEFAULT'),
-        '&:hover': {
-          color: 'var(--tw-prose-links-hover)',
-          textDecorationColor: 'var(--tw-prose-links-underline)',
-        },
-      },
-      ':is(h1, h2, h3) a': {
-        fontWeight: 'inherit',
-      },
-      strong: {
-        color: 'var(--tw-prose-bold)',
-        fontWeight: '600',
-      },
-      ':is(a, blockquote, thead th) strong': {
-        color: 'inherit',
-      },
-      code: {
-        color: 'var(--tw-prose-code)',
-        borderRadius: theme('borderRadius.lg'),
-        paddingTop: theme('padding.1'),
-        paddingRight: theme('padding[1.5]'),
-        paddingBottom: theme('padding.1'),
-        paddingLeft: theme('padding[1.5]'),
-        boxShadow: 'inset 0 0 0 1px var(--tw-prose-code-ring)',
-        backgroundColor: 'var(--tw-prose-code-bg)',
-        fontSize: theme('fontSize.2xs'),
-      },
-      ':is(a, h1, h2, h3, blockquote, thead th) code': {
-        color: 'inherit',
-      },
-      'h2 code': {
-        fontSize: theme('fontSize.base')[0],
-        fontWeight: 'inherit',
-      },
-      'h3 code': {
-        fontSize: theme('fontSize.sm')[0],
-        fontWeight: 'inherit',
-      },
-
-      // Overrides
-      ':is(h1, h2, h3) + *': {
-        marginTop: '0',
-      },
-      '> :first-child': {
-        marginTop: '0 !important',
-      },
-      '> :last-child': {
-        marginBottom: '0 !important',
-      },
-    },
-  },
-  invert: {
-    css: {
-      '--tw-prose-body': 'var(--tw-prose-invert-body)',
-      '--tw-prose-headings': 'var(--tw-prose-invert-headings)',
-      '--tw-prose-links': 'var(--tw-prose-invert-links)',
-      '--tw-prose-links-hover': 'var(--tw-prose-invert-links-hover)',
-      '--tw-prose-links-underline': 'var(--tw-prose-invert-links-underline)',
-      '--tw-prose-bold': 'var(--tw-prose-invert-bold)',
-      '--tw-prose-counters': 'var(--tw-prose-invert-counters)',
-      '--tw-prose-bullets': 'var(--tw-prose-invert-bullets)',
-      '--tw-prose-hr': 'var(--tw-prose-invert-hr)',
-      '--tw-prose-quotes': 'var(--tw-prose-invert-quotes)',
-      '--tw-prose-quote-borders': 'var(--tw-prose-invert-quote-borders)',
-      '--tw-prose-captions': 'var(--tw-prose-invert-captions)',
-      '--tw-prose-code': 'var(--tw-prose-invert-code)',
-      '--tw-prose-code-bg': 'var(--tw-prose-invert-code-bg)',
-      '--tw-prose-code-ring': 'var(--tw-prose-invert-code-ring)',
-      '--tw-prose-th-borders': 'var(--tw-prose-invert-th-borders)',
-      '--tw-prose-td-borders': 'var(--tw-prose-invert-td-borders)',
     },
   },
 })
